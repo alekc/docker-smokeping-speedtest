1e7763d59ab1e5465c78d156bfb184ec1a4dcb27
Alexander Chernov <alexander@chernov.it>
2026-02-17 20:24:55 +0000

    Add caching mechanism for speedtest results
    Implemented a caching system to store and retrieve speedtest results, reducing the need for repeated tests. Added methods for cache key generation, reading, and writing cache data. The cache validity period is set to 60 seconds by default. Updated the speedtest execution logic to utilize cached results when available.

--- speedtest.pm
+++ speedtest.pm
@@ -19,7 +19,10 @@ use base qw(Smokeping::probes::basefork);
 use IPC::Open3;
 use Symbol;
 use Carp;
-use Sys::Syslog qw(:standard :macros);;
+use Sys::Syslog qw(:standard :macros);
+use Fcntl qw(:flock);
+use File::Spec;
+use Digest::MD5 qw(md5_hex);
 
 sub pod_hash {
 	return {
@@ -47,6 +50,122 @@ openlog("speedtest", "nofatal, pid", "local0");
 #set to LOG_ERR to disable debugging, LOG_DEBUG to enable debugging
 setlogmask(LOG_MASK(LOG_ERR));
 
+# Cache directory for storing speedtest results
+my $CACHE_DIR = $ENV{'SMOKEPING_SPEEDTEST_CACHE'} || '/tmp/smokeping-speedtest-cache';
+# Cache validity period in seconds (default: 60 seconds)
+my $CACHE_TTL = 60;
+
+sub _get_cache_key {
+    my ($self, $target) = @_;
+    my $server = $target->{vars}{server} || "default";
+    my $binary = $self->{properties}{binary};
+    # Create a unique key based on server and binary path
+    return md5_hex("$binary-$server");
+}
+
+sub _get_cache_path {
+    my ($self, $cache_key) = @_;
+    mkdir($CACHE_DIR, 0755) unless -d $CACHE_DIR;
+    return File::Spec->catfile($CACHE_DIR, "$cache_key.cache");
+}
+
+sub _read_cache {
+    my ($self, $target) = @_;
+
+    my $cache_key = $self->_get_cache_key($target);
+    my $cache_file = $self->_get_cache_path($cache_key);
+
+    return undef unless -f $cache_file;
+
+    # Try to open and lock the cache file
+    if (open(my $fh, '<', $cache_file)) {
+        flock($fh, LOCK_SH) or do {
+            close($fh);
+            return undef;
+        };
+
+        # Read cache content
+        my $content = do { local $/; <$fh> };
+        flock($fh, LOCK_UN);
+        close($fh);
+
+        # Parse cache: timestamp|json_data
+        if ($content =~ /^(\d+)\|(.+)$/s) {
+            my ($timestamp, $json) = ($1, $2);
+            my $age = time() - $timestamp;
+
+            if ($age < $CACHE_TTL) {
+                $self->do_debug("[Speedtest] Cache hit (age: ${age}s)\n");
+                syslog("debug", "[Speedtest] Cache hit for key $cache_key (age: ${age}s)");
+                return $json;
+            } else {
+                $self->do_debug("[Speedtest] Cache expired (age: ${age}s)\n");
+                syslog("debug", "[Speedtest] Cache expired for key $cache_key (age: ${age}s)");
+            }
+        }
+    }
+
+    return undef;
+}
+
+sub _write_cache {
+    my ($self, $target, $json_data) = @_;
+
+    my $cache_key = $self->_get_cache_key($target);
+    my $cache_file = $self->_get_cache_path($cache_key);
+
+    # Open with exclusive lock for writing
+    if (open(my $fh, '>', $cache_file)) {
+        flock($fh, LOCK_EX) or do {
+            close($fh);
+            return 0;
+        };
+
+        my $timestamp = time();
+        print $fh "$timestamp|$json_data";
+
+        flock($fh, LOCK_UN);
+        close($fh);
+
+        $self->do_debug("[Speedtest] Cache written\n");
+        syslog("debug", "[Speedtest] Cache written for key $cache_key");
+        return 1;
+    }
+
+    return 0;
+}
+
+sub _run_speedtest {
+    my ($self, $target) = @_;
+
+    my $server = $target->{vars}{server} || undef;
+    my $extra = $target->{vars}{extraargs} || "";
+    my $query = "$self->{properties}{binary} ".((defined($server))?"--server-id=$server":"")." -f json --accept-license --accept-gdpr $extra 2>&1";
+
+    $self->do_debug("[Speedtest] Running: $query\n");
+    syslog("debug", "[Speedtest] Running: $query");
+
+    my $inh = gensym;
+    my $outh = gensym;
+    my $errh = gensym;
+
+    my $pid = open3($inh, $outh, $errh, $query);
+    my $json_output = '';
+
+    while (<$outh>) {
+        $self->do_debug("[Speedtest] output: ".$_);
+        syslog("debug", "[Speedtest] output: ".$_);
+        $json_output .= $_;
+    }
+
+    waitpid $pid, 0;
+    close $errh;
+    close $inh;
+    close $outh;
+
+    return $json_output;
+}
+
 sub new($$$)
 {
     my $proto = shift;
@@ -59,7 +178,7 @@ sub new($$$)
         #check for dependencies
         my $call = "$self->{properties}{binary} --version";
         my $return = `$call 2>&1`;
-        if ($return =~ /speedtest ([0-9]+\.[0-9]+(?:\.[0-9]+)?(?:\.[0-9]+)?)/){
+        if ($return =~ /speedtest.*?([0-9]+\.[0-9]+(?:\.[0-9]+)?(?:\.[0-9]+)?)/i){
             print "### parsing $self->{properties}{binary} output... OK (version $1)\n";
             syslog("debug", "[Speedtest] Init: version $1");
         } else {
@@ -117,66 +236,55 @@ sub pingone ($){
     my $self = shift;
     my $target = shift;
 
-    my $inh = gensym;
-    my $outh = gensym;
-    my $errh = gensym;
-
-    my $server = $target->{vars}{server} || undef; #if server is not provided, use the default one recommended by speedtest.
-    my $measurement = $target->{vars}{measurement} || "download"; #record download speeds if nothing is returned
-    my $extra = $target->{vars}{extraargs} || ""; #append extra arguments if neded
-    my $query = "$self->{properties}{binary} ".((defined($server))?"--server-id=$server":"")." -f json --accept-license --accept-gdpr $extra 2>&1";
-
+    my $measurement = $target->{vars}{measurement} || "download";
     my @times;
 
-    $self->do_debug("query=$query\n");
-    syslog("debug", "[Speedtest] query=$query");
-#    for (my $run = 0; $run < $self->pings($target); $run++) {
-	my $pid = open3($inh,$outh,$errh, $query);
-	while (<$outh>) {
-        $self->do_debug("output: ".$_);
-        syslog("debug", "[Speedtest] output: ".$_);
+    # Try to get cached result first
+    my $json_output = $self->_read_cache($target);
 
-        # Parse JSON output from Ookla speedtest
-        # {"ping":{"jitter":0.42,"latency":5.123},"download":{"bandwidth":112140288,"bytes":1234567,"elapsed":10001},"upload":{"bandwidth":56070144,"bytes":654321,"elapsed":9876}}
+    # If no cache or cache expired, run speedtest
+    if (!defined $json_output) {
+        $self->do_debug("[Speedtest] No valid cache, running speedtest\n");
+        syslog("debug", "[Speedtest] No valid cache, running speedtest");
 
-        if ($measurement eq "ping") {
-            # Extract ping latency in milliseconds
-            my ($value) = /"ping":\{"jitter":[0-9.]+,"latency":([0-9.]+)/;
-            if (defined $value) {
-                my $normalizedvalue = $value; # already in milliseconds
-                $self->do_debug("Got value: $value ms -> $normalizedvalue\n");
-                syslog("debug","[Speedtest] Got value: $value ms -> $normalizedvalue\n");
-                push @times, $normalizedvalue;
-                last;
-            }
-        } elsif ($measurement eq "download") {
-            # Extract download bandwidth in bytes/sec and convert to bits/sec
-            my ($value) = /"download":\{"bandwidth":([0-9]+)/;
-            if (defined $value) {
-                my $normalizedvalue = $value * 8; # convert bytes/sec to bits/sec
-                $self->do_debug("Got value: $value bytes/s -> $normalizedvalue bits/s\n");
-                syslog("debug","[Speedtest] Got value: $value bytes/s -> $normalizedvalue bits/s\n");
-                push @times, $normalizedvalue;
-                last;
-            }
-        } elsif ($measurement eq "upload") {
-            # Extract upload bandwidth in bytes/sec and convert to bits/sec
-            my ($value) = /"upload":\{"bandwidth":([0-9]+)/;
-            if (defined $value) {
-                my $normalizedvalue = $value * 8; # convert bytes/sec to bits/sec
-                $self->do_debug("Got value: $value bytes/s -> $normalizedvalue bits/s\n");
-                syslog("debug","[Speedtest] Got value: $value bytes/s -> $normalizedvalue bits/s\n");
-                push @times, $normalizedvalue;
-                last;
-            }
+        $json_output = $self->_run_speedtest($target);
+
+        # Cache the result for future use
+        $self->_write_cache($target, $json_output);
+    }
+
+    # Parse the JSON output based on measurement type
+    if ($measurement eq "ping") {
+        # Extract ping latency in milliseconds
+        if ($json_output =~ /"ping":\s*\{\s*"jitter":\s*[0-9.]+\s*,\s*"latency":\s*([0-9.]+)/s) {
+            my $value = $1;
+            my $normalizedvalue = $value; # already in milliseconds
+            $self->do_debug("[Speedtest] Got ping value: $value ms -> $normalizedvalue\n");
+            syslog("debug","[Speedtest] Got ping value: $value ms -> $normalizedvalue\n");
+            push @times, $normalizedvalue;
         }
-	}
-	waitpid $pid,0;
-	close $errh;
-	close $inh;
-	close $outh;
-#    }
-    #we run only one test (in order not to get banned too soon), so we ignore pings and have to return the correct number of values. Uncomment the above for loop if you want the actual testing to be done $ping times.
+    } elsif ($measurement eq "download") {
+        # Extract download bandwidth in bytes/sec and convert to bits/sec
+        if ($json_output =~ /"download":\s*\{\s*"bandwidth":\s*([0-9]+)/s) {
+            my $value = $1;
+            my $normalizedvalue = $value * 8; # convert bytes/sec to bits/sec
+            $self->do_debug("[Speedtest] Got download value: $value bytes/s -> $normalizedvalue bits/s\n");
+            syslog("debug","[Speedtest] Got download value: $value bytes/s -> $normalizedvalue bits/s\n");
+            push @times, $normalizedvalue;
+        }
+    } elsif ($measurement eq "upload") {
+        # Extract upload bandwidth in bytes/sec and convert to bits/sec
+        if ($json_output =~ /"upload":\s*\{\s*"bandwidth":\s*([0-9]+)/s) {
+            my $value = $1;
+            my $normalizedvalue = $value * 8; # convert bytes/sec to bits/sec
+            $self->do_debug("[Speedtest] Got upload value: $value bytes/s -> $normalizedvalue bits/s\n");
+            syslog("debug","[Speedtest] Got upload value: $value bytes/s -> $normalizedvalue bits/s\n");
+            push @times, $normalizedvalue;
+        }
+    }
+
+    # We run only one test (in order not to get banned too soon), so we ignore pings
+    # and have to return the correct number of values.
     my $value = $times[0];
     @times = ();
     for(my $run = 0; $run < $self->pings($target); $run++) {
@@ -185,7 +293,7 @@ sub pingone ($){
 
     @times = map {sprintf "%.10e", $_ } sort {$a <=> $b} grep {$_ ne "-"} @times;
 
-    $self->do_debug("time=@times\n");
+    $self->do_debug("[Speedtest] time=@times\n");
     syslog("debug", "[Speedtest] time=@times");
     return @times;
 }
